6a50748062fb80db62dd55ac1bee332e8ac86358
aryan <aryansatija2003@gmail.com>
2024-08-25 13:33:17 +0530

    gauge chart

--- web/client/src/components/GaugeChart.tsx
+++ web/client/src/components/GaugeChart.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+interface GaugeChartProps {
+  value : number | undefined;
+}
 
-const GaugeChart = () => {
+const GaugeChart = ({ value } : GaugeChartProps) => {
   return (
-    <div>GaugeChart</div>
+    <Gauge
+      value={value ? value : 0}
+      startAngle={-110}
+      endAngle={110}
+      height={400}
+      sx={{
+        [`& .${gaugeClasses.valueText}`]: {
+          fontSize: 40,
+          transform: 'translate(0px, 0px)',
+        },
+      }}
+      text={
+        ({ value, valueMax }) => `${value} / ${valueMax}`
+      }
+    />
   )
 }
 
--- web/client/src/pages/analytics.tsx
+++ web/client/src/pages/analytics.tsx
@@ -4,6 +4,8 @@ import Paper from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
 import { styled } from '@mui/material/styles';
 import EngineeringIcon from '@mui/icons-material/Engineering';
+import GaugeChart from '../components/GaugeChart';
+import { blueGrey } from '@mui/material/colors';
 
 const Label = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -21,7 +23,7 @@ export function Analytics() {
       <Masonry columns={3} spacing={2}>
         {itemData.map((item, index) => (
           <Paper key={index}>
-            <div className='flex flex-col items-center justify-center pb-4'>{index + 1}</div>
+            <div className='flex flex-col items-center justify-center pb-4'>{item?.title}</div>
             {
               item.type === 'unknown' && 
               <div className='flex h-[300px] flex-col items-center justify-center text-4xl text-slate-600 pb-4 font-bold'>
@@ -32,7 +34,13 @@ export function Analytics() {
               </div>
             }
             {
-              item.type !== 'unknown' && 
+              item.type === 'gauge chart' && 
+              <div className='flex flex-col items-center justify-center pb-4 font-bold'>
+                  <GaugeChart value={item?.data}/>
+              </div>
+            }
+            {
+              item.type !== 'unknown' && item.type !== 'gauge chart' && 
               <img
                 srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
                 src={`${item.img}?w=162&auto=format`}
@@ -62,7 +70,8 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
     title: 'AI Alert Accuracy',
-    type: 'gauge chart'
+    type: 'gauge chart',
+    data: 80
   },
   {
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
