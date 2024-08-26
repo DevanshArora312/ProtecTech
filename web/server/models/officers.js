d0e69e73eaa7462fab489e25aa8ada94fa72baf0
aryan <aryansatija2003@gmail.com>
2024-08-27 01:40:56 +0530

    officer model

---
+++ web/server/models/officers.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const officerSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastname: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    mobile: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 12,
+        maxlength: 12
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    thana_id: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Officer", officerSchema);
