e78e2957281517e938143a0dd93a7541ede575c4
aryan <aryansatija2003@gmail.com>
2024-09-07 21:29:59 +0530

    radar card

--- web/client/src/pages/detect.tsx
+++ web/client/src/pages/detect.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import Radar from '../components/radar';
 const Detect = () => {
     const {latitude, longitude} = useParams();
 
     return (
-        <div>Detect</div>
+        <div className='flex flex-col h-full items-center justify-center'>
+            <Radar/>
+        </div>
     )
 }
 
