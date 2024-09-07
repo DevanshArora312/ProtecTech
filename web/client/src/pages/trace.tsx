490cabee48143963418d889f7f8cba0b87356b1d
aryan <aryansatija2003@gmail.com>
2024-09-08 01:18:37 +0530

    trace page

--- web/client/src/pages/trace.tsx
+++ web/client/src/pages/trace.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { MapPreviewCard } from '../components/mapPreview'
+import { MapPreviewCard } from '../components/mapPreview';
+import { useParams } from 'react-router-dom';
 const Trace = () => {
+    const {latitude, longitude} = useParams();
+
   return (
-    <div>
-        <MapPreviewCard longitude={20} latitude={20}/>
+    <div className='w-full h-[85vh] flex flex-col items-center justify-center'>
+        <MapPreviewCard longitude={latitude} latitude={longitude}/>
     </div>
   )
 }
